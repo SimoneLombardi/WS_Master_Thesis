75619587557c0150a4a2bbfb0e09ff08c8247a5e
Simone <lombardi.simone.unige@gmail.com>
2026-02-09 11:11:14 +0100

    commit clean, from my pc

--- src/uc1_robot_controllers/launch/sensing_control.launch.py
+++ src/uc1_robot_controllers/launch/sensing_control.launch.py
@@ -30,8 +30,8 @@ def generate_launch_description():
         executable="interactive-target",
         name="ik_target_body_marker",
         parameters=[{
-            "position":[2.3, -1.7, 0.05],
-            "orientation":[0.0, 0.0, -1.57]
+            "position":[3.0, 0.0, 0.05],
+            "orientation":[0.0, 0.0, 0.0]
         }]
     )
     
@@ -45,7 +45,7 @@ def generate_launch_description():
         package="pointcloud_interactive_markers",
         executable="obstacle-cloud-broadcaster",
         parameters=[{
-            "ptc_filename":"/resources/chassis_downsampled_cloud_scaled_centered.pcd",
+            "ptc_filename":"/resources/single_cylinder_colud.pcd",
             "ptc_pkgname":"environment_pointcloud_processing"
         }]
     )
